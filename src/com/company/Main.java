deedcceaeef10d051d3271ce545122f615aff7d6
Zina-ha <Aki.Zi@mail.ru>
2020-07-26 17:13:53 +0300

    Dev

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,31 @@
 package com.company;
 
+import java.io.File;
+import java.util.Set;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args)
+    {
+
+        String folderPath = "C:\\Users\\lenovo\\Desktop\\База";
+        File file = new File(folderPath);
+
+        System.out.println(getFolderSize(file));
+
+    }
+    public static long getFolderSize(File folder)
+    {
+      if (folder.isFile())    {
+          return folder.length();
+      }
+      long sum = 0;
+      File[] files = folder.listFiles();
+      for(File file : files)
+      {
+          sum += getFolderSize(file);
+
+      }
+      return sum;
     }
 }
